3528055f14a3241b68fe64f1e0a423d3a702c8f1
pyy2114 <pyy2114@naver.com>
2023-06-01 04:48:19 +0900

    Second Commit

--- movieProject01/src/movieProject01/Main.java
+++ movieProject01/src/movieProject01/Main.java
@@ -8,30 +8,31 @@ public class Main {
 		// TODO Auto-generated method stub
 		int num = 0;
 				
-				Scanner sc = new Scanner(System.in);
-				Manager M = new Manager();
-				
-				
-				do {
-					//메인 선택창
-					M.displayMainMenu();
-					System.out.printf("번호를 입력해 주세요: ");
-					num = sc.nextInt();
-					
-					switch (num) {
-						case 1:	//예매
-							M.ticekt();
-							break;
-						case 2:	//추천
-							M.recommand();
-							break;
-						case 3:	//확인
-							M.confirm();
-							break;
-						default:	//끝
-							break;	
-					}
-				}while(num != 4);
+		Scanner sc = new Scanner(System.in);
+		Manager M = new Manager();
+		
+		
+		do {
+			//메인 선택창
+			M.displayMainMenu();
+			System.out.printf("번호를 입력해 주세요: ");
+			num = sc.nextInt();
+			
+			switch (num) {
+				case 1:	//예매
+					M.ticekt();
+					break;
+				case 2:	//추천
+					M.recommand();
+					break;
+				case 3:	//확인
+					M.confirm();
+					break;
+				default:	//끝
+					break;	
+			}
+		}while(num != 4);
+	
 	}
 
 }
